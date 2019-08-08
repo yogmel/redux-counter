2b65dbea548a9e487711699ba2b194586f7c9bfb
Mell Y <pong7219@gmail.com>
2019-08-08 11:32:04 -0300

    Setup em Redux de store, actions e reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+
+const incrementFunc = (valor = 1) => ({
+  type: 'INCREMENT',
+  valor
+})
+
+const decrementFunc = (valor = 1) => ({
+  type: 'DECREMENT',
+  valor
+})
+
+const resetFunc = () => ({
+  type: 'RESET'
+})
+
+const reducer = ( state = 0, action ) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + action.valor
+    case 'DECREMENT':
+      return state - action.valor
+    case 'RESET':
+      return 0
+    default:
+      return state.counter;
+  }
+}
+
+const store = createStore(reducer)
+
+store.dispatch(incrementFunc())
+store.dispatch(incrementFunc())
+store.dispatch(incrementFunc())
+store.dispatch(decrementFunc(5))
+store.dispatch(resetFunc())
+
+console.log(store.getState())
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teste</h1>
     </div>
   );
 }
